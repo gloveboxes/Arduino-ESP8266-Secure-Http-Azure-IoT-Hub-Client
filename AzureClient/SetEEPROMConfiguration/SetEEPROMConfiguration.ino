7b8f366985b429b3886dfc4a565724034b01641d
mtnrbq <rmawston@live.com.au>
2016-01-22 07:43:37 +1100

    Changes to support single wifi ap

--- AzureClient/SetEEPROMConfiguration/SetEEPROMConfiguration.ino
+++ AzureClient/SetEEPROMConfiguration/SetEEPROMConfiguration.ino
@@ -1,17 +1,23 @@
 #include <ArduinoJson.h>
 #include <EEPROM.h>
 
-int wifi = 2;  // number of defined wifi ssid/pwd pairs
-const char* ssid[] = { "NCW", "dgTether" };
-const char* password[] = { "malolos5459", "VisualStudio2005" };
+int numWifi = 1;  // number of defined wifi ssid/pwd pairs
+const char* ssid[] = { "ss1" }; // any additonal go here  ,"ss2" };
+const char* password[] = { "pw1" }; // any additonal go here  ,"pw2" };
 const char* geo = "-33.8745828,151.2067465";  // Sydney Town Hall
+const char* ssidi;
+const char* pwdi;
 
 
 // IOT Hub
-const char* host = "MakerDen.azure-devices.net";
-const char* id = "WeMos01";
-const char* key = "phKNWLHKTVcn0iF+Plb4kGkohPenDzLg/jFSRyX9s/g=";
+const char* host = "MYIOTHUB.azure-devices.net";
+const char* id = "MYDEVICEID";
+const char* key = "base64encodedkey";
 
+/*
+Id,PrimaryKey,SecondaryKey,ConnectionString,ConnectionState,LastActivityTime,LastConnectionStateUpdatedTime,LastStateUpdatedTime,MessageCount,State,SuspensionReason
+rmesp8266-01,r/3oR5vRacvNdebhW0cIj1rdPAKlEjbDCJ0KfHWxnLk=,juiokEvl2LYzaS+Jlov8wvhe9afDVsd7jr5/5ZEUTLY=,HostName=sdcdemohub.azure-devices.net;DeviceId=rmesp8266-01;SharedAccessKey=r/3oR5vRacvNdebhW0cIj1rdPAKlEjbDCJ0KfHWxnLk=,Disconnected,1/01/0001 12:00:00 AM,1/01/0001 12:00:00 AM,1/01/0001 12:00:00 AM,0,Enabled,
+*/
 
 // Event Hub
 //const char* host = "makerdenww-ns.servicebus.windows.net";
@@ -26,80 +32,86 @@ int length;
 
 
 void setup() {
-  Serial.begin(9600);
-  EEPROM.begin(512);
+	Serial.begin(9600);
+	EEPROM.begin(512);
 
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  
-  //root["SSID"] = ssid;
-   root["wifi"] = wifi;
+	StaticJsonBuffer<1000> jsonBuffer;
+	JsonObject& root = jsonBuffer.createObject();
 
-  JsonArray& _ssid = root.createNestedArray("ssid");
-  _ssid.add(ssid[0]);
-  _ssid.add(ssid[1]);
+	root["wifi"] = numWifi;
 
-  JsonArray& _password = root.createNestedArray("pwd");
-  _password.add(password[0]);
-  _password.add(password[1]);
+	JsonArray& _ssid = root.createNestedArray("ssid");
+	for (int i = 0; i < numWifi; i++)
+	{
+		_ssid.add(ssid[i]);
+	}
 
-  root["host"] = host;
-  root["key"] = key;
-  root["id"] = id;
-  root["geo"] = geo;
-  
-  length = root.printTo(buffer, BufferLen);
-  root.prettyPrintTo(Serial);
-  
-  EEPROM.write(0, highByte(length));
-  EEPROM.write(1, lowByte(length));
-  for (int address = 2; address < length + 2; address++){
-    EEPROM.write(address, buffer[address-2]);
-  }
-  EEPROM.commit();
+	JsonArray& _password = root.createNestedArray("pwd");
+	for (int i = 0; i < numWifi; i++)
+	{
+		_password.add(password[i]);
+	}
+
+
+	root["host"] = host;
+	root["key"] = key;
+	root["id"] = id;
+	root["geo"] = geo;
+
+	length = root.printTo(buffer, BufferLen);
+	root.prettyPrintTo(Serial);
+
+	EEPROM.write(0, highByte(length));
+	EEPROM.write(1, lowByte(length));
+	for (int address = 2; address < length + 2; address++) {
+		EEPROM.write(address, buffer[address - 2]);
+	}
+	EEPROM.commit();
 }
 
 void loop() {
 
-  getconfiguration();
-  delay(2000);
-  
+	getconfiguration();
+	delay(2000);
+
 }
 
-void getconfiguration(){
-  StaticJsonBuffer<1000> jsonBuffer;
-  int address = 2;
-  length = word(EEPROM.read(0), EEPROM.read(1));
-  for (address = 2; address < length + 2; address++) {
-    buffer[address-2] = EEPROM.read(address);
-  }
-  buffer[address-2] = '\0';
-  Serial.println(buffer);
+void getconfiguration() {
+	StaticJsonBuffer<1000> jsonBuffer;
+	int address = 2;
+	length = word(EEPROM.read(0), EEPROM.read(1));
+	for (address = 2; address < length + 2; address++) {
+		buffer[address - 2] = EEPROM.read(address);
+	}
+	buffer[address - 2] = '\0';
+	Serial.println(buffer);
 
-  JsonObject& root = jsonBuffer.parseObject(buffer);
-  if (!root.success())
-  {
-    Serial.println("parseObject() failed");
-    return;
-  }
+	JsonObject& root = jsonBuffer.parseObject(buffer);
+	if (!root.success())
+	{
+		Serial.println("parseObject() failed");
+		return;
+	}
 
-  wifi = root["wifi"];
-  ssid[0] = root["ssid"][0];
-  password[0] = root["pwd"][0];
-  host = root["host"];
-  key = root["key"];
-  id = root["id"];
-  geo = root["geo"];
+	numWifi = root["wifi"];
+	host = root["host"];
+	key = root["key"];
+	id = root["id"];
+	geo = root["geo"];
 
-  Serial.println(wifi);
-  Serial.println(ssid[0]);
-  Serial.println(password[0]);
-  Serial.println(ssid[1]);
-  Serial.println(password[1]);
-  Serial.println(host);
-  Serial.println(key);
-  Serial.println(id);
-  Serial.println(geo);
+	Serial.println(numWifi);
+	for (size_t i = 0; i < numWifi; i++)
+	{
+		ssidi = root["ssid"][i];
+		pwdi = root["pwd"][i];
+		Serial.print("["); Serial.print(i); Serial.print("]");
+		Serial.print(ssidi); Serial.print(" - ");
+		Serial.println(pwdi);
+	}
+	Serial.println(host);
+	Serial.println(key);
+	Serial.println(id);
+	Serial.println(geo);
 }
 
 
--- AzureClient/SetEEPROMConfiguration/__vm/.SetEEPROMConfiguration.vsarduino.h
+++ AzureClient/SetEEPROMConfiguration/__vm/.SetEEPROMConfiguration.vsarduino.h
@@ -5,7 +5,7 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: NodeMCU 1.0 (ESP-12E Module), Platform=esp8266, Package=esp8266
+	Hardware: WeMos D1 R2 & mini, Platform=esp8266, Package=esp8266com
 */
 
 #ifndef _VSARDUINO_H_
@@ -15,10 +15,11 @@
 #define __ets__
 #define ICACHE_FLASH
 #define F_CPU 80000000L
-#define ARDUINO 165
+#define ARDUINO 10607
 #define ARDUINO_ESP8266_ESP12
 #define ARDUINO_ARCH_ESP8266
 #define __cplusplus
+#define __ARM__
 #define __inline__
 #define __asm__(x)
 #define __extension__
@@ -97,5 +98,5 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portOutputRegister(P)
 #define portInputRegister(P)
 #define portModeRegister(P)
-#include <SetEEPROMConfiguration.ino>
+#include <..\SetEEPROMConfiguration\SetEEPROMConfiguration.ino>
 #endif
